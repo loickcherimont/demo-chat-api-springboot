3980411aeb32c44962427aab01d82cbd4785e714
loickcherimont <loickcherimont@gmail.com>
2026-03-20 22:22:30 +0100

    feat: add default fake users into H2 database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,12 @@
+-- Messages
 INSERT INTO 
     messages(name)
 VALUES('Hello from PostgreSQL');
+
+-- Users
+
+INSERT INTO
+    users(username, password)
+VALUES
+    ('john.doe', 'test123'),
+    ('jane.doe', 'user123');
