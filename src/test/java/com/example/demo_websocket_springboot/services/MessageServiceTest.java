6be7af05d3b108cffd25f34d1c78d1f69d2011b2
loickcherimont <loickcherimont@gmail.com>
2026-04-04 01:12:30 +0200

    refactor: remove unused Greeting object

--- src/test/java/com/example/demo_websocket_springboot/services/MessageServiceTest.java
+++ src/test/java/com/example/demo_websocket_springboot/services/MessageServiceTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.example.demo_websocket_springboot.model.Greeting;
+// import com.example.demo_websocket_springboot.model.Greeting;
 import com.example.demo_websocket_springboot.model.Message;
 
 class MessageServiceTest {
