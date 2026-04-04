10c55c45a1d5b72c2e9f7baa2a90fefa1cab4ddb
loickcherimont <loickcherimont@gmail.com>
2026-04-04 09:42:14 +0200

    store stomp/websocket services into the same `stompClient` object

--- src/main/resources/static/scripts/websocket.js
+++ src/main/resources/static/scripts/websocket.js
@@ -6,32 +6,26 @@ const wsProtocol = window.location.protocol === "https:" ? "wss" : "ws";
 const stompClient = new StompJs.Client({
     brokerURL: `${wsProtocol}://${window.location.host}/chat-app`,
     reconnectDelay: 5000,
+    beforeConnect: async () => {
+        jwtToken = localStorage.getItem("CUSTOM_JWT_TOKEN");
+        stompClient.connectHeaders = {
+            Authorization: `Bearer ${jwtToken}`,
+        };
+    },
+    onConnect: async () => {
+        const listMessages = await loadMessages(`${window.location.origin}/api/messages`, jwtToken);
+        listMessages.forEach(listItemMessage => createMessageElement(listItemMessage));
+        stompClient.subscribe("/topic/messages", (message) => {
+            createMessageElement(JSON.parse(message.body));
+        });
+    },
+    onWebSocketError: (error) => console.error("error with websocket: ", error),
+    onStompError: (error) => (frame) => {
+        console.error(`Broker reported error: ${frame.headers["message"]}`);
+        console.error(`Additional details: ${frame.body}`);
+        redirectToAuthForm();
+    }
 });
-
-stompClient.beforeConnect = async () => {
-    jwtToken = localStorage.getItem("CUSTOM_JWT_TOKEN");
-    stompClient.connectHeaders = {
-        Authorization: `Bearer ${jwtToken}`,
-    };
-};
-
-stompClient.onConnect = async () => {
-    await loadMessages("/api/messages", jwtToken);
-    stompClient.subscribe("/topic/messages", (message) => {
-        createMessageElement(JSON.parse(message.body));
-    });
-};
-
-// Handle errors on STOMP environment
-stompClient.onWebSocketError = (error) => {
-    console.error("Error with websocket", error);
-};
-
-stompClient.onStompError = (frame) => {
-    console.error(`Broker reported error: ${frame.headers["message"]}`);
-    console.error(`Additional details: ${frame.body}`);
-    redirectToAuthForm();
-};
 
 export async function connect() {
     try {
