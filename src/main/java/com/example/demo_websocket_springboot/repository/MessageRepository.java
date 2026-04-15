ba9e0765c58d050dd25a7ba24792d4541178b78e
loickcherimont <loickcherimont@gmail.com>
2026-04-15 19:41:43 +0200

    refactor: remove the whitespace

--- src/main/java/com/example/demo_websocket_springboot/repository/MessageRepository.java
+++ src/main/java/com/example/demo_websocket_springboot/repository/MessageRepository.java
@@ -6,6 +6,4 @@ import org.springframework.stereotype.Repository;
 import com.example.demo_websocket_springboot.model.Message;
 
 @Repository
-public interface MessageRepository extends JpaRepository<Message, Long> {
-
-}
+public interface MessageRepository extends JpaRepository<Message, Long> {}
