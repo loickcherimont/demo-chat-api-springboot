4d4f27564582a5d665b3dfce5fe0bab1334c0970
loickcherimont <loickcherimont@gmail.com>
2026-04-25 13:06:33 +0200

    refactor: remove unused `log`

--- src/main/java/com/example/demo_websocket_springboot/controller/AuthController.java
+++ src/main/java/com/example/demo_websocket_springboot/controller/AuthController.java
@@ -1,12 +1,6 @@
 package com.example.demo_websocket_springboot.controller;
 
-import java.util.Map;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,7 +23,6 @@ public class AuthController {
 
     @PostMapping("/signin")
     public ResponseEntity<SigninResponseDto> signin(@RequestBody SigninRequestDto dto) {
-        log.info("Signin requested for {}", dto.username());
         return ResponseEntity.ok(authService.signin(dto));
     }
 }
